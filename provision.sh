60ff2365b22d196c4c2e6296388545ebdac44d07
fsh-nur <129324316+fsh-nur@users.noreply.github.com>
2023-04-18 11:51:30 +0100

    Create provision.sh

---
+++ provision.sh
@@ -0,0 +1,14 @@
+## The code should be inputted in the shell script
+```
+#!/bin/bash
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt install nginx -y
+
+sudo systemctl restart nginx
+
+sudo systemctl enable nginx
+
+```
